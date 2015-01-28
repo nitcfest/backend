c4a0356f6c91e6895a3e6149ec731e554de975ca
Saneem <xaneem@gmail.com>
2015-01-28 16:33:59 +0530

    Initial - laravel install

---
+++ app/config/production/app.php
@@ -0,0 +1,9 @@
+<?php
+
+return array(
+
+	'debug' => false,
+	'url' => 'http://www.ragam.org.in/2015/api/',
+	'timezone' => 'Asia/Kolkata',
+
+);
---
+++ app/config/production/database.php
@@ -0,0 +1,20 @@
+<?php
+
+return array(
+
+	'connections' => array(
+
+		'mysql' => array(
+			'driver'    => 'mysql',
+			'host'      => 'localhost',
+			'database'  => 'db_name',
+			'username'  => 'db_user',
+			'password'  => 'db_pass',
+			'charset'   => 'utf8',
+			'collation' => 'utf8_unicode_ci',
+			'prefix'    => '',
+		)
+
+	)
+
+);
