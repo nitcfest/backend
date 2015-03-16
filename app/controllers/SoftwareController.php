ca8ac1fa51f2b6a1f9ca195a0c62d186d475fad6
Saneem <xaneem@gmail.com>
2015-03-16 21:55:36 +0530

    Removed nitc from hospitality

--- app/controllers/SoftwareController.php
+++ app/controllers/SoftwareController.php
@@ -4,8 +4,12 @@
 class SoftwareController extends BaseController {
 
 	public function hospitality(){
-		$males = Registration::where('hospitality_type','=', 1)->orderBy('id','asc')->get();
-		$females = Registration::where('hospitality_type','=', 2)->orderBy('id','asc')->get();
+
+		//ID of NIT Calicut in colleges table
+		$nit_id = 2;
+
+		$males = Registration::where('hospitality_type','=', 1)->where('college_id','!=',$nit_id)->orderBy('id','asc')->get();
+		$females = Registration::where('hospitality_type','=', 2)->where('college_id','!=',$nit_id)->orderBy('id','asc')->get();
 
 		return View::make('software.hospitality', array('males'=>$males, 'females'=>$females));
 	}
